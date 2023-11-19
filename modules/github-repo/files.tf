7712688c900bcc26ddb4c6671e1442271f086a2c
Héctor Castelli Zacharias <hector.zacharias@gmail.com>
2023-11-19 12:12:40 +0100

    feat: initialize vscode on repositories (#3)

--- modules/github-repo/files.tf
+++ modules/github-repo/files.tf
@@ -22,3 +22,43 @@ resource "github_repository_file" "readme" {
     ]
   }
 }
+
+resource "github_repository_file" "vscode_settings" {
+  repository          = github_repository.repository.name
+  branch              = github_branch_default.default.branch
+  file                = ".vscode/settings.json"
+  content             = file("${path.module}/files/.vscode/settings.json.tftpl")
+  commit_message      = "chore(vscode.settings): Initialize with template"
+  commit_author       = "InfrastructureAsCode"
+  commit_email        = "infrastructure@powerd6.org"
+  overwrite_on_create = true
+
+  lifecycle {
+    ignore_changes = [
+      content,
+      commit_message,
+      commit_author,
+      commit_email
+    ]
+  }
+}
+
+resource "github_repository_file" "vscode_extensions" {
+  repository          = github_repository.repository.name
+  branch              = github_branch_default.default.branch
+  file                = ".vscode/extensions.json"
+  content             = file("${path.module}/files/.vscode/extensions.json.tftpl")
+  commit_message      = "chore(vscode.extensions): Initialize with basic extensions"
+  commit_author       = "InfrastructureAsCode"
+  commit_email        = "infrastructure@powerd6.org"
+  overwrite_on_create = true
+
+  lifecycle {
+    ignore_changes = [
+      content,
+      commit_message,
+      commit_author,
+      commit_email
+    ]
+  }
+}
