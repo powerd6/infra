be193d954f0995b43812d58b6c551aa55018234e
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2023-11-19 19:36:11 +0100

    fix: remove spec environments

--- stages/repositories/spec.tf
+++ stages/repositories/spec.tf
@@ -5,27 +5,3 @@ module "specification" {
   topics             = ["specification", "documentation"]
   org_administrators = var.org_administrators
 }
-
-resource "github_repository_environment" "spec_preview" {
-  environment = "preview"
-  repository  = module.specification.name
-  deployment_branch_policy {
-    protected_branches     = true
-    custom_branch_policies = false
-  }
-}
-
-resource "github_repository_environment" "spec_release" {
-  environment = "release"
-  repository  = module.specification.name
-  deployment_branch_policy {
-    protected_branches     = false
-    custom_branch_policies = true
-  }
-}
-
-resource "github_repository_environment_deployment_policy" "spec_release_main" {
-  repository     = module.specification.name
-  environment    = github_repository_environment.spec_release.environment
-  branch_pattern = module.specification.main_branch
-}
