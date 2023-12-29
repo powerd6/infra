e974b6238f6a706b61680ee5cebd65fb5acda1c7
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2023-12-29 23:27:12 +0100

    feat: add spec gh pages challenge record

--- stages/repositories/spec.tf
+++ stages/repositories/spec.tf
@@ -7,6 +7,14 @@ module "spec" {
   org_administrators = var.org_administrators
 }
 
+resource "gandi_livedns_record" "spec_gh-pages-challenge" {
+  name   = "_github-pages-challenge-powerd6.spec"
+  ttl    = 10800
+  type   = "TXT"
+  values = ["812e1a3eee1c1521399fa597195914"]
+  zone   = var.domain
+}
+
 resource "gandi_livedns_record" "spec_gh-pages-cname" {
   name   = module.spec.name
   ttl    = 10800
