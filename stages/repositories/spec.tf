f3d1c08faaa80877666eb5e05150e9ecadb7afe3
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2023-12-30 00:02:43 +0100

    fix gandi dns issue

--- stages/repositories/spec.tf
+++ stages/repositories/spec.tf
@@ -3,7 +3,7 @@ module "spec" {
   name               = "spec"
   description        = "This is the powerd6 specification, and it documents how the project behaves."
   topics             = ["specification", "documentation"]
-  pages_cname        = "https://spec.powerd6.org"
+  pages_cname        = "spec.powerd6.org"
   org_administrators = var.org_administrators
 }
 
@@ -11,7 +11,7 @@ resource "gandi_livedns_record" "spec_gh-pages-challenge" {
   name   = "_github-pages-challenge-powerd6.spec"
   ttl    = 10800
   type   = "TXT"
-  values = ["812e1a3eee1c1521399fa597195914"]
+  values = ["\"812e1a3eee1c1521399fa597195914\""]
   zone   = var.domain
 }
 
