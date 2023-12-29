dd8882761a495b5645edf51ca160340a8f09b636
Héctor Castelli Zacharias <hector.zacharias@gmail.com>
2023-12-29 23:04:21 +0000

    fix: spec github pages environment (#9)
    * add github environment to spec
    
    * fix gandi dns issue

--- stages/repositories/spec.tf
+++ stages/repositories/spec.tf
@@ -3,7 +3,7 @@ module "spec" {
   name               = "spec"
   description        = "This is the powerd6 specification, and it documents how the project behaves."
   topics             = ["specification", "documentation"]
-  pages_cname        = "https://spec.powerd6.org"
+  pages_cname        = "spec.powerd6.org"
   org_administrators = var.org_administrators
 }
 
@@ -11,7 +11,7 @@ resource "gandi_livedns_record" "spec_gh-pages-challenge" {
   name   = "_github-pages-challenge-powerd6.spec"
   ttl    = 10800
   type   = "TXT"
-  values = ["812e1a3eee1c1521399fa597195914"]
+  values = ["\"812e1a3eee1c1521399fa597195914\""]
   zone   = var.domain
 }
 
@@ -21,4 +21,19 @@ resource "gandi_livedns_record" "spec_gh-pages-cname" {
   type   = "CNAME"
   values = ["powerd6.github.io."]
   zone   = var.domain
+}
+
+resource "github_repository_environment" "spec_github_pages" {
+  environment = "github-pages"
+  repository  = module.spec.name
+  deployment_branch_policy {
+    protected_branches     = false
+    custom_branch_policies = true
+  }
+}
+
+resource "github_repository_environment_deployment_policy" "spec_github_pages_releases" {
+  repository     = module.spec.name
+  environment    = github_repository_environment.spec_github_pages.environment
+  branch_pattern = "v*"
 }
